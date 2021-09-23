284898d558f3339f0f80ddc44022d0d859e0b1bf
Alejandro Clara <aleclara95@gmail.com>
2021-09-23 20:03:35 -0300

    Added backend url to .env

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api/v1/'
+    baseURL: process.env.REACT_APP_BACKEND_API_BASE_URL
 });
 
 export default axiosInstance;
